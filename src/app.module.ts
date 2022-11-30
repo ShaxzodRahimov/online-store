07b48eed279c26f6b6d31b19028f0a23849a5154
Shahzod <shahzodrahimov1234@gmail.com>
2022-11-30 13:07:15 +0500

    added intial routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { SubcategoriesModule } from './subcategories/subcategories.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule, SubcategoriesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
